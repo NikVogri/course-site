17bf32059070e971100320fd16c0c73694334fa5
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-05-15 01:07:53 +0200

    deployment error fix #1

--- src/components/playlistItem.jsx
+++ src/components/playlistItem.jsx
@@ -5,7 +5,7 @@ import { Link } from "gatsby";
 
 const playlistItem = ({ active, title, videoId }) => {
   return (
-    <Link to="videoTemplate" state={{ videoId }}>
+    <Link to={"/videoTemplate"} state={{ videoId }}>
       <div className={`playlist-item ${active ? "active" : ""}`}>
         <input type="checkbox" />
         <img
