b65b6cfc6f54383b35b47af3ad5d56b4ef4709de
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-05-15 00:57:16 +0200

    dynamic course pages

--- src/components/playlistItem.jsx
+++ src/components/playlistItem.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { Link } from "gatsby";
 
-import Placeholder from "../images/placeholder.png";
+// import Placeholder from "../images/placeholder.png";
 
-const playlistItem = ({ active }) => {
+const playlistItem = ({ active, title, videoId }) => {
   return (
-    <div className={`playlist-item ${active ? "active" : ""}`}>
-      <input type="checkbox" />
-      <img src={Placeholder} alt="placeholder" />
-      <span>
-        2: Basic PHP Syntax | PHP Tutorial | PHP For Beginners | Learn PHP
-        Programming
-      </span>
-    </div>
+    <Link to="videoTemplate" state={{ videoId }}>
+      <div className={`playlist-item ${active ? "active" : ""}`}>
+        <input type="checkbox" />
+        <img
+          src={`https://img.youtube.com/vi/${videoId}/0.jpg`}
+          alt="placeholder"
+        />
+        <span>{title}</span>
+      </div>
+    </Link>
   );
 };
 
--- src/pages/videoTemplate.js
+++ src/pages/videoTemplate.js
@@ -10,12 +10,57 @@ import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
-const VideoTemplate = () => {
+const tempVideoData = [
+  {
+    id: 1,
+    videoId: "XBj_le81sAc",
+    title:
+      "1: Introduction To PHP | Procedural PHP Tutorial For Beginners | PHP Tutorial | mmtuts",
+  },
+  {
+    id: 2,
+    videoId: "U10yvfIStx8",
+    title:
+      "2: Basic Syntax In PHP | Procedural PHP Tutorial For Beginners | PHP Tutorial | mmtuts",
+  },
+  {
+    id: 3,
+    videoId: "atNrwSTB3-c",
+    title:
+      "3: Scalar Data Types In PHP | Procedural PHP Tutorial For Beginners | PHP Tutorial | mmtuts",
+  },
+  {
+    id: 4,
+    videoId: "DiEfNQsapbc",
+    title:
+      "4: Variables In PHP | Procedural PHP Tutorial For Beginners | PHP Tutorial | mmtuts",
+  },
+  {
+    id: 5,
+    videoId: "WPYCJg9OSq4",
+    title:
+      "5: Expressions in PHP | Procedural PHP Tutorial For Beginners | PHP Tutorial | mmtuts",
+  },
+  {
+    id: 6,
+    videoId: "dx7dO-pkGKg",
+    title:
+      "6: Operators In PHP | Procedural PHP Tutorial For Beginners | PHP Tutorial | mmtuts",
+  },
+];
+
+const VideoTemplate = ({ location }) => {
   const [tab, setTab] = useState(1);
   const [collapse, setCollapse] = useState(false);
+  const { videoId } = location.state;
 
   const collapseHandler = () => {
     setCollapse(!collapse);
+  };
+
+  const tabHandler = tab => {
+    setCollapse(false);
+    setTab(tab);
   };
 
   return (
@@ -26,10 +71,10 @@ const VideoTemplate = () => {
           <iframe
             width="100%"
             height="100%"
-            src="https://www.youtube.com/embed/pHxBA2ZbHis"
-            frameborder="0"
+            src={`https://www.youtube.com/embed/${videoId}`}
+            frameBorder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-            allowfullscreen
+            allowFullScreen
             title="video"
           ></iframe>
         </div>
@@ -53,17 +98,17 @@ const VideoTemplate = () => {
             )}
           </li>
           <li className={`${tab === 1 && "active"}`}>
-            <button onClick={() => setTab(1)} className="btn-reset">
+            <button onClick={() => tabHandler(1)} className="btn-reset">
               Description
             </button>
           </li>
           <li className={`${tab === 2 && "active"}`}>
-            <button onClick={() => setTab(2)} className="btn-reset">
+            <button onClick={() => tabHandler(2)} className="btn-reset">
               My Notes
             </button>
           </li>
           <li className={`${tab === 3 && "active"}`}>
-            <button onClick={() => setTab(3)} className="btn-reset">
+            <button onClick={() => tabHandler(3)} className="btn-reset">
               Comments
             </button>
           </li>
@@ -71,9 +116,15 @@ const VideoTemplate = () => {
         <Info tab={tab} handleCollapse={collapseHandler} collapse={collapse} />
       </section>
       <section className="playlist">
-        <PlaylistItem />
-        <PlaylistItem active />
-        <PlaylistItem />
+        {tempVideoData &&
+          tempVideoData.map(content => (
+            <PlaylistItem
+              title={content.title}
+              videoId={content.videoId}
+              active={videoId === content.videoId}
+              key={content.id}
+            />
+          ))}
       </section>
     </Layout>
   );
